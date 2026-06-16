5efca48848bc122334dd7046cb6334135f4600d6
unknown <bhadurituhina@gmail.com>
2026-06-16 11:18:39 +0530

    Retrieving information conditionally

--- database_test.py
+++ database_test.py
@@ -4,7 +4,8 @@ connection = sqlite3.connect("scan_history.db")
 
 cursor = connection.cursor()
 
-cursor.execute("SELECT * FROM scan_history")
+cursor.execute('''SELECT * FROM scan_history
+               WHERE timestamp >= '2023-01-01' ''')
 records = cursor.fetchall()
 print("Database records: ", records)
 
