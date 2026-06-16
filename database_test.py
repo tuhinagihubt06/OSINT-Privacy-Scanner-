f2719c3b949252ca79866d6420ddc918193c0b71
unknown <bhadurituhina@gmail.com>
2026-06-16 11:14:06 +0530

    Retrieve records from SQLite database

--- database_test.py
+++ database_test.py
@@ -4,13 +4,8 @@ connection = sqlite3.connect("scan_history.db")
 
 cursor = connection.cursor()
 
-cursor.execute('''
-               INSERT INTO scan_history (username, score, risk, timestamp)
-               VALUES (?, ?, ?, ?)
-''', ('john_doe', 85, 'High', '2023-10-01 12:00:00'))
-
-connection.commit()
-
-print("Data inserted successfully!")
+cursor.execute("SELECT * FROM scan_history")
+records = cursor.fetchall()
+print("Database records: ", records)
 
 connection.close()
